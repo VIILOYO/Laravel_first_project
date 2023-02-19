436f5a8918bea02fa792cfcd094ffbc266eb6003
Alexey Babenko <viiloyo@gmail.com>
2023-02-19 23:25:30 +0700

    Route Groups

---
+++ app/Http/Controllers/TestsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Test;
+
+class TestsController extends Controller
+{
+    public function index() {
+        return view('tests.index', ['tests' => Test::where('is_active', 1)->get()]);
+    }
+
+    public function create() {
+        return view('tests.create');
+    }
+
+    public function store(Request $request) {
+        $data = $request->only(['title', 'description']);
+        $test = Test::create($data);
+
+        return redirect("tests/$test->id");
+    }
+
+    public function show($id) {
+        return view('tests.show', ['test' => Test::findOrFail($id)]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,21 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PostsController;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\TestsController;
 
 
 Route::get('/', [Controller::class, 'index']);
 
-Route::get('/posts', [PostsController::class, 'index' ]);
-Route::get('/posts/create', [PostsController::class, 'create']);
-Route::get('/posts/{id}', [PostsController::class, 'show']);
-Route::post('/posts', [PostsController::class, 'store']);
+Route::controller(PostsController::class)->group(function() {
+    Route::get('/posts',  'index');
+    Route::get('/posts/create', 'create');
+    Route::get('/posts/{id}', 'show');
+    Route::post('/posts', 'store');
+});
 
-Route::get('/tests', [TestController::class, 'index']);
-Route::get('/tests/create', [TestController::class, 'create']);
-Route::get('tests/{id}', [TestController::class, 'show']);
-Route::post('/tests', [TestController::class, 'store']);
+Route::controller(TestsController::class)->group(function() {
+    Route::get('/tests', 'index');
+    Route::get('/tests/create', 'create');
+    Route::get('/tests/{id}', 'show');
+    Route::post('/tests', 'store');
+});
