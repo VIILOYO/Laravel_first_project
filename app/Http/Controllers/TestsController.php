d45eb2af7727830f08d45f0eb293233509e22359
Alexey Babenko <viiloyo@gmail.com>
2023-02-22 12:07:40 +0700

    resource controller по правилам

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -2,43 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\Test;
+use Illuminate\View\View;
 
 class TestsController extends Controller
 {
-    public function index() {
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
         return view('tests.index', ['tests' => Test::where('is_active', 1)->get()]);
     }
 
-    public function create() {
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
         return view('tests.create');
     }
 
-    public function store(Request $request) {
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
         $data = $request->only(['title', 'description']);
         $test = Test::create($data);
 
         return redirect("tests/$test->id");
     }
 
-    public function show($id) {
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): View
+    {
         return view('tests.show', ['test' => Test::findOrFail($id)]);
     }
 
-    public function destroy($id) {
-        Test::findOrFail($id)->delete();
-        return redirect()->route('tests.index');
-    }
-
-    public function edit($id) {
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): View
+    {
         return view('tests.edit', ['test' => Test::findOrFail($id)]);
     }
 
-    public function update($id, Request $request) {
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): RedirectResponse
+    {
         $data = $request->only('title', 'description');
         Test::findOrFail($id)->update($data);
 
         return redirect("tests/$id");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): RedirectResponse
+    {
+        Test::findOrFail($id)->delete();
+        return redirect()->route('tests.index');
     }
 }
