fdecb71918524a841c0621f6eacb467639479331
Alexey Babenko <viiloyo@gmail.com>
2023-03-02 14:49:37 +0700

    Создана система аутентификация случайно)
    Подключен bootstrap:

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,21 @@ use App\Http\Controllers\TestsController;
 use App\Http\Controllers\FriendsController;
 use App\Http\Controllers\MyPlaceController;
 
+Route::get('/', function() {
+    return view('main');
+});
 
-Route::get('/posts', [PostsController::class, 'index']);
+Route::get('/posts', [PostsController::class, 'index'])->name('posts');
 Route::get('/posts/{id}', [PostsController::class, 'post'])->whereNumber('id');
 Route::get('/posts/likest', [PostsController::class, 'likest']);
 
-Route::get('friends', [FriendsController::class, 'index']);
+Route::get('friends', [FriendsController::class, 'index'])->name('friends');
 Route::get('friends/{id}', [FriendsController::class, 'friend'])->whereNumber('id');
 Route::get('friends/dead', [FriendsController::class, 'deadPeople']);
 
-Route::get('tests', [TestsController::class, 'index']);
+Route::get('tests', [TestsController::class, 'index'])->name('tests');
 Route::get('tests/{id}', [TestsController::class, 'test']);
 
-Route::get('places', [MyPlaceController::class, 'index']);
+Route::get('places', [MyPlaceController::class, 'index'])->name('places');
 Route::get('places/{id}', [MyPlaceController::class, 'place'])->whereNumber('id');
 Route::get('places/high', [MyPlaceController::class, 'highRating']);
